c2bcde620caab7822ab14b52694663a65b659ec4
Anusan <1522411@stud.hs-mannheim.de>
2018-12-03 19:11:12 +0100

    FlightPopulator finished ... compilation errors due to missing Enumeration SeatType

---
+++ flightsystem/src/main/java/de/hsma/igt/flightsystem/tools/FlightPopulator.java
@@ -0,0 +1,35 @@
+package de.hsma.igt.flightsystem.tools;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import de.hsma.igt.flightsystem.models.Flight;
+import de.hsma.igt.flightsystem.models.FlightSeats;
+import de.hsma.igt.flightsystem.tools.RandomGenerator;
+
+public class FlightPopulator {
+	
+	public static List<Flight> populateCustomerAsList(int numberOfCustomers) {
+		
+		List<Flight> flights = new ArrayList<Flight>();
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			Flight flight = new Flight();
+			String planeType = RandomGenerator.getRandomAString(100);
+			FlightSeats flightSeatsEconomy = new FlightSeats(50, 50.0, flight, SeatType.ECONOMY);
+			FlightSeats flightSeatsBusiness = new FlightSeats(50, 50.0, flight, SeatType.BUSINESS);
+			
+			List<FlightSeats> list = Arrays.asList(flightSeatsBusiness, flightSeatsEconomy);
+			Set<FlightSeats> flightSeats = new HashSet<>(list);
+			
+			
+			flights.add(flight);
+		}
+		
+		return flights;
+	}
+
+}
