c1d8b8f9cb487b191d3b0d4dc0ef3916ff105cf3
Anusan <1522411@stud.hs-mannheim.de>
2018-12-04 11:18:29 +0100

    refactor Enum

--- flightsystem/src/main/java/de/hsma/igt/flightsystem/tools/PersistenceUnit.java
+++ flightsystem/src/main/java/de/hsma/igt/flightsystem/tools/PersistenceUnit.java
@@ -1,5 +1,5 @@
 package de.hsma.igt.flightsystem.tools;
 
 public enum PersistenceUnit {
-    OGM_MYSQL, OGM_POSTGRESQL, OGM_MONGODB, OGM_NEO4J, OGM_INFINISPAN, OGM_CASSANDRA, OGM_REDIS,
+    OGM_MYSQL, OGM_MONGODB, OGM_NEO4J, OGM_CASSANDRA, OGM_REDIS
 }
