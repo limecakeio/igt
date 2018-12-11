48171a265594e5c9c4b0655c4254d7b9dac48c8e
Anusan <1522411@stud.hs-mannheim.de>
2018-12-11 11:45:36 +0100

    final tests

--- flightsystem/src/main/java/de/hsma/igt/flightsystem/tools/Config.java
+++ flightsystem/src/main/java/de/hsma/igt/flightsystem/tools/Config.java
@@ -6,7 +6,7 @@ public class Config {
 	public static final String LOG = System.getProperty("user.dir") + "/logs/";
     public static final String LOG_STORAGE_LOCATION = "_crud_performance.txt";
     public static final Integer TRANSACTION_TIMEOUT = 9000000;
-    public static final String PERSIST_STORAGE_LOCATION = System.getProperty("user.dir") + "/logs/customers.txt";
+    public static final String PERSIST_STORAGE_LOCATION = System.getProperty("user.dir") + "/logs/performance.txt";
     public static final String PERSIST_STORAGE_OUTPUT_LOCATION = System.getProperty("user.dir") + "/logs/customers_out.txt";
 
 }
